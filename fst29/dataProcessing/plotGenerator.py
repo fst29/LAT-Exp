75e43611fe2fe4b54c062725edfab045857a558c
fst29 <fst29@cam.ac.uk>
2024-03-11 09:37:00 +0000

    Changed plot generator to create double plots

--- fst29/dataProcessing/plotGenerator.py
+++ fst29/dataProcessing/plotGenerator.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 
 dataFile = "C:/Users/ftief/OneDrive - University of Cambridge/Cambridge/Part IIB/IIB Project/LAT-Exp/fst29/processedData/Dynamic friction/forFitting.csv"
-outputFolder = "C:/Users/ftief/OneDrive - University of Cambridge/Cambridge/Part IIB/IIB Project/LAT-Exp/fst29/processedData/Dynamic friction/plots/"
+outputFolder = "C:/Users/ftief/OneDrive - University of Cambridge/Cambridge/Part IIB/IIB Project/Final report/Dynamic friction plots/"
 
 
 TIME = 0
@@ -39,31 +39,49 @@ with open(dataFile) as f:
 for speed in speeds:
     for p in pvalues:
         for offset in offsets:
+            
+            
             x = []
+            againstInput = []
+            againstOutput = []
             measured = []
             quadratic = []
             sinusoidal = []
             print(speed, p, offset)
             for row in inRangePoints:
                 if speed in row[COMMAND][:-4] and "1"+speed not in row[COMMAND][:-4] and abs(float(row[CARRIAGEPOS]) - p) <= 0.15 and float(row[OFFSET]) == offset:
-                    #x.append(float(row[OUTPUTPOS]))
-                    x.append(float(row[INPUTPOS]))
+                    againstOutput.append(float(row[OUTPUTPOS]))
+                    againstInput.append(float(row[INPUTPOS]))
                     measured.append(float(row[FRICTION]))
                     quadratic.append(float(row[QUADRATIC]))
                     sinusoidal.append(float(row[SINUSOIDAL]))
 
             print(len(x))
 
-            ax = plt.axes()
+             #plt.figure()
+            #ax = plt.axes()
+            fig, (ax1, ax2) = plt.subplots(1, 2, sharey=True, figsize=(8, 4))
+            ax1.scatter(againstInput, measured, label="Measured")
+            ax1.scatter(againstInput, quadratic, label="Quadratic fit")
+            ax1.scatter(againstInput, sinusoidal, label="Sinusoidal fit")
+            ax1.set_xlabel("Primary shaft position (deg)")
+            ax1.set_ylabel("Friction (Nm)")
+            ax1.yaxis.tick_right()
+            ax1.yaxis.set_label_position("right")
+            ax1.legend()
 
-            ax.scatter(x, measured, label="Measured")
-            ax.scatter(x, quadratic, label="Quadratic fit")
-            ax.scatter(x, sinusoidal, label="Sinusoidal fit")
-            ax.set_xlabel("Primary shaft position (deg)")
-            ax.set_ylabel("Friction (Nm)")
-            ax.legend()
+            ax2.scatter(againstOutput, measured, label="Measured")
+            ax2.scatter(againstOutput, quadratic, label="Quadratic fit")
+            ax2.scatter(againstOutput, sinusoidal, label="Sinusoidal fit")
+            ax2.set_xlabel("Secondary shaft position (deg)")
+            #ax2.set_ylabel("Friction (Nm)")
+            ax2.legend()
+
+            
             title = f"speed: {speed}, p: {p}, offset: {offset}"
             filename = f"speed{speed}, p{p}, offset{offset}"
-            ax.set_title(title)
-            plt.savefig(outputFolder+"FvsI"+filename+".png")
-            plt.clf()
+            fig.suptitle(title)
+            plt.subplots_adjust(bottom=0.11,left=0.01, right=0.99, top=0.93)
+            #plt.set_size_cm()
+            plt.savefig(outputFolder+"dynFric"+filename+".png")
+            #plt.show()
